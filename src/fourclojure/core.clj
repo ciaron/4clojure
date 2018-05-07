f6424902b41c95d600b2eca4de8943d75e027b36
Ciaron Linstead <linstead@pik-potsdam.de>
2018-05-07 13:38:05 +0200

    initial commit

---
+++ src/fourclojure/core.clj
@@ -0,0 +1,14 @@
+(ns fourclojure.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
+
+;; 4clojure exercises
+;; Ex 1 - Nothing but the Truth
+(= true true)
+
+;; Ex 2 - Simple Math
+
